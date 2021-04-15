5e9db20f096aeaf321e80b12d84561077e0840d8
Эвелина Маркова <esmarkova_1@miem.hse.ru>
2021-04-15 22:48:26 +0300

    Your most sworn enemies

---
+++ ghosts.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 22:32:47 2021
+
+@author: eveli
+"""
+
+import pygame
+from movement import Entity
+from const import *
+from vector import Vector2
+from random import randint
+from chasing import Mode
+from stack import Stack
+
+class Ghost(Entity):
+    def __init__(self, nodes):
+        Entity.__init__(self, nodes)
+        self.name = "ghost"
+        self.goal = Vector2()
+        self.points = 200
+        self.modeStack = self.setupModeStack()
+        self.mode = self.modeStack.pop()
+        self.modeTimer = 0
+
+    def getValidDirections(self):
+        validDirections = []
+        for key in self.node.neighbors.keys():
+            if self.node.neighbors[key] is not None:
+                if key != self.direction * -1:
+                    validDirections.append(key)
+        if len(validDirections) == 0:
+            validDirections.append(self.forceBacktrack())
+        return validDirections
+        
+    def randomDirection(self, validDirections):
+        index = randint(0, len(validDirections) - 1)
+        return validDirections[index]
+    
+    def getClosestDirection(self, validDirections):
+        distances = []
+        for direction in validDirections:
+            diffVec = self.node.position + direction*TILEWIDTH - self.goal
+            distances.append(diffVec.magnitudeSquared())
+        index = distances.index(min(distances))
+        return validDirections[index]
+
+    def moveBySelf(self):
+        if self.overshotTarget():
+            self.node = self.target
+            self.portal()
+            validDirections = self.getValidDirections()
+            self.direction = self.getClosestDirection(validDirections)
+            self.target = self.node.neighbors[self.direction]
+            self.setPosition()
+
+    def forceBacktrack(self):
+        if self.direction * -1 == UP:
+            return UP
+        if self.direction * -1 == DOWN:
+            return DOWN
+        if self.direction * -1 == LEFT:
+            return LEFT
+        if self.direction * -1 == RIGHT:
+            return RIGHT
+           
+    def portalSlowdown(self):
+        self.speed = 100
+        if self.node.portalNode or self.target.portalNode:
+            self.speed = 50
+            
+    def setupModeStack(self):
+        modes = Stack()
+        modes.push(Mode(name="CHASE"))
+        modes.push(Mode(name="SCATTER", time=5))
+        modes.push(Mode(name="CHASE", time=20))
+        modes.push(Mode(name="SCATTER", time=7))
+        modes.push(Mode(name="CHASE", time=20))
+        modes.push(Mode(name="SCATTER", time=7))
+        modes.push(Mode(name="CHASE", time=20))
+        modes.push(Mode(name="SCATTER", time=7))
+        return modes
+
+    def scatterGoal(self):
+        self.goal = Vector2(SCREENSIZE[0], 0)
+
+    def chaseGoal(self, pacman, blinky=None):
+        self.goal = pacman.position
+
+    def modeUpdate(self, dt):
+        self.modeTimer += dt
+        if self.mode.time is not None:
+            if self.modeTimer >= self.mode.time:
+                self.reverseDirection()
+                self.mode = self.modeStack.pop()
+                self.modeTimer = 0
+
+    def update(self, dt, pacman, blinky=None):
+        self.visible = True
+        self.portalSlowdown()
+        speedMod = self.speed * self.mode.speedMult
+        self.position += self.direction*speedMod*dt
+        self.modeUpdate(dt)
+        if self.mode.name == "CHASE":
+            self.chaseGoal(pacman, blinky)
+        elif self.mode.name == "SCATTER":
+            self.scatterGoal()
+        self.moveBySelf()
+       
+
+                

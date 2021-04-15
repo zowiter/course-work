57c5a5e367c3136a5267c57686fcaec5a3370da1
Эвелина Маркова <esmarkova_1@miem.hse.ru>
2021-04-15 22:48:52 +0300

    Just movement as they should be

---
+++ movement.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 22:23:25 2021
+
+@author: eveli
+"""
+
+import pygame
+from vector import Vector2
+from const import *
+
+class Entity(object):
+    def __init__(self, nodes):
+        self.name = ""
+        self.direction = STOP
+        self.speed = 100
+        self.radius = 10
+        self.collideRadius = 5
+        self.color = WHITE
+        self.nodes = nodes
+        self.node = nodes.nodeList[0]
+        self.target = self.node
+        self.setPosition()
+        self.visible = True
+
+    def setPosition(self):
+        self.position = self.node.position.copy()
+
+    def update(self, dt):
+        self.position += self.direction*self.speed*dt
+        self.moveBySelf()
+
+    def moveBySelf(self):
+        if self.direction is not STOP:
+            if self.overshotTarget():
+                self.node = self.target
+                self.portal()
+                if self.node.neighbors[self.direction] is not None:
+                    self.target = self.node.neighbors[self.direction]
+                else:
+                    self.setPosition()
+                    self.direction = STOP
+
+    def overshotTarget(self):
+        if self.target is not None:
+            vec1 = self.target.position - self.node.position
+            vec2 = self.position - self.node.position
+            node2Target = vec1.magnitudeSquared()
+            node2Self = vec2.magnitudeSquared()
+            return node2Self >= node2Target
+        return False
+    
+    def reverseDirection(self):
+        if self.direction is UP: self.direction = DOWN
+        elif self.direction is DOWN: self.direction = UP
+        elif self.direction is LEFT: self.direction = RIGHT
+        elif self.direction is RIGHT: self.direction = LEFT
+        temp = self.node
+        self.node = self.target
+        self.target = temp
+
+    def portal(self):
+        if self.node.portalNode:
+            self.node = self.node.portalNode
+            self.setPosition()
+
+    def render(self, screen):
+        if self.visible:
+            p = self.position.asInt()
+            pygame.draw.circle(screen, self.color, p, self.radius)

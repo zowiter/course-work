1f998fc5223371a5d144c8826a9352cc291edad2
Эвелина Маркова <esmarkova_1@miem.hse.ru>
2021-05-13 22:41:58 +0300

    Your most sworn enemies

--- ghosts.py
+++ ghosts.py
@@ -23,6 +23,7 @@ class Ghost(Entity):
         self.mode = self.modeStack.pop()
         self.modeTimer = 0
 
+
     def getValidDirections(self):
         validDirections = []
         for key in self.node.neighbors.keys():
@@ -32,11 +33,11 @@ class Ghost(Entity):
         if len(validDirections) == 0:
             validDirections.append(self.forceBacktrack())
         return validDirections
-        
+
     def randomDirection(self, validDirections):
         index = randint(0, len(validDirections) - 1)
         return validDirections[index]
-    
+
     def getClosestDirection(self, validDirections):
         distances = []
         for direction in validDirections:
@@ -63,12 +64,12 @@ class Ghost(Entity):
             return LEFT
         if self.direction * -1 == RIGHT:
             return RIGHT
-           
+
     def portalSlowdown(self):
         self.speed = 100
         if self.node.portalNode or self.target.portalNode:
             self.speed = 50
-            
+
     def setupModeStack(self):
         modes = Stack()
         modes.push(Mode(name="CHASE"))
@@ -106,6 +107,115 @@ class Ghost(Entity):
         elif self.mode.name == "SCATTER":
             self.scatterGoal()
         self.moveBySelf()
-       
 
-                
+    def setStartPosition(self):
+        self.node = self.findStartNode()
+        self.target = self.node
+        self.setPosition()
+
+class Blinky(Ghost):
+    def __init__(self, nodes):
+        Ghost.__init__(self, nodes)
+        self.name = "blinky"
+        self.color = RED
+        self.setStartPosition()
+
+    def findStartNode(self):
+        for node in self.nodes.nodeList:
+            if node.blinkyStartNode:
+                return node
+        return None
+
+class Pinky(Ghost):
+    def __init__(self, nodes):
+        Ghost.__init__(self, nodes)
+        self.name = "pinky"
+        self.color = PINK
+        self.setStartPosition()
+
+    def findStartNode(self):
+        for node in self.nodes.nodeList:
+            if node.pinkyStartNode:
+                return node
+        return None
+
+    def scatterGoal(self):
+        self.goal = Vector2()
+
+    def chaseGoal(self, pacman, blinky=None):
+        self.goal = pacman.position + pacman.direction * TILEWIDTH * 4
+
+class Inky(Ghost):
+    def __init__(self, nodes):
+        Ghost.__init__(self, nodes)
+        self.name = "inky"
+        self.color = TEAL
+        self.setStartPosition()
+
+    def findStartNode(self):
+        for node in self.nodes.nodeList:
+            if node.inkyStartNode:
+                return node
+        return None
+
+    def scatterGoal(self):
+        self.goal = Vector2(TILEWIDTH*NCOLS, TILEHEIGHT*NROWS)
+
+    def chaseGoal(self, pacman, blinky=None):
+        vec1 = pacman.position + pacman.direction * TILEWIDTH * 2
+        vec2 = (vec1 - blinky.position) * 2
+        self.goal = blinky.position + vec2
+
+class Clyde(Ghost):
+    def __init__(self, nodes):
+        Ghost.__init__(self, nodes)
+        self.name = "clyde"
+        self.color = ORANGE
+        self.setStartPosition()
+
+    def findStartNode(self):
+        for node in self.nodes.nodeList:
+            if node.clydeStartNode:
+                return node
+        return None
+
+    def scatterGoal(self):
+        self.goal = Vector2(0, TILEHEIGHT*NROWS)
+
+    def chaseGoal(self, pacman, blinky=None):
+        d = pacman.position - self.position
+        ds = d.magnitudeSquared()
+        if ds <= (TILEWIDTH * 8)**2:
+            self.scatterGoal()
+        else:
+            self.goal = pacman.position + pacman.direction * TILEWIDTH * 4
+
+class GhostGroup(object):
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.ghosts = [Blinky(nodes), Pinky(nodes), Inky(nodes), Clyde(nodes)]
+
+    def __iter__(self):
+        return iter(self.ghosts)
+
+    def update(self, dt, pacman):
+        for ghost in self:
+            ghost.update(dt, pacman, self.ghosts[0])
+
+    def freightMode(self):
+        for ghost in self:
+            ghost.freightMode()
+
+    def updatePoints(self):
+        for ghost in self:
+            ghost.points *= 2
+
+    def resetPoints(self):
+        for ghost in self:
+            ghost.points = 200
+
+    def render(self, screen):
+        for ghost in self:
+            ghost.render(screen)
+
+
